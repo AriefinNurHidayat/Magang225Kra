d7ec7e807734ac9405e3058793f22d9f623767f9
AriefinNurHidayat <l200220031@student.ums.ac.id>
2025-05-05 11:22:03 +0700

    login dan register

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,14 @@ Route::get('/ruang', function(){
     return view('pages.ruangan');
 });
 
+Route::get('/login', function(){
+    return view('login');
+});
+
+Route::get('/register', function(){
+    return view('register');
+});
+
 Route::get('/pemesanan', function(){
     return view('pages.pemesanan');
 });
