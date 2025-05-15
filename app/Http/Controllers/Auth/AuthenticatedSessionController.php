2ac87dc51359494f81455355388106630c09bea2
AriefinNurHidayat <l200220031@student.ums.ac.id>
2025-05-15 11:53:58 +0700

    updateanbaru

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use Illuminate\Validation\ValidationException;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -22,14 +23,33 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
-    {
-        $request->authenticate();
+    // public function store(LoginRequest $request): RedirectResponse
+    // {
+    //     $request->authenticate();
 
-        $request->session()->regenerate();
+    //     $request->session()->regenerate();
 
-        return redirect()->intended(route('main', absolute: false));
+    //     return redirect()->intended(route('main', absolute: false));
+    // }
+
+    public function store(Request $request): RedirectResponse
+{
+    $request->validate([
+        'email' => ['required', 'string', 'email'],
+        'password' => ['required', 'string'],
+    ]);
+
+    if (!Auth::attempt($request->only('email', 'password'), $request->boolean('remember'))) {
+        throw ValidationException::withMessages([
+            'email' => __('auth.failed'),
+        ]);
     }
+
+    $request->session()->regenerate();
+
+    // Redirect sesuai role
+    return redirect()->route(Auth::user()->role === 'admin' ? 'admin' : 'main');
+}
 
     /**
      * Destroy an authenticated session.
--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -7,25 +7,19 @@ use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
 
-
-class AdminMiddleware
+class RoleMiddleware
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle($request, Closure $next, $role)
     {
-        if (!Auth::check()) {
-            return redirect()->route('login');
-        }
-    
-        if (Auth::user()->role === 'admin') {
+        if (Auth::check() && Auth::user()->role === $role) {
             return $next($request);
-            
         }
-    
-        abort(403, 'Unauthorized');
+
+        abort(403, 'Akses ditolak.');
     }
 }
